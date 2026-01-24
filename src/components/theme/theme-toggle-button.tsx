defecc0520508518f79614ed335ff4ee099e6ba1
Christopher Anderson <src@christopheranderson.net>
2026-01-23 19:18:52 -0500

    bug: turn theme toggle autocomplete off (#23)

--- src/components/theme/theme-toggle-button.tsx
+++ src/components/theme/theme-toggle-button.tsx
@@ -5,39 +5,44 @@ import {
   ToggleLeftIcon,
   ToggleRightIcon,
 } from '@phosphor-icons/react/dist/ssr'
+import { Form } from '../ui/forms'
 import { useThemeToggle } from './theme-provider'
 import type { SwitchProps } from 'react-aria-components'
 
 const ThemeToggleButton = (props: SwitchProps) => {
   const { theme, toggleTheme } = useThemeToggle()
   const isThemeSet = theme !== undefined
+  // autocomplete explicitly off to avoid hydration mismatch issues in firefox
+  // https://bugzilla.mozilla.org/show_bug.cgi?id=654072#c4
   return (
-    <div
-      css={{
-        display: 'inline-flex',
-        alignItems: 'center',
-        gap: '4px',
-        transition: 'opacity 0.3s ease',
-      }}
-      style={{ opacity: isThemeSet ? 1 : 0.3 }}
-      aria-hidden={!isThemeSet}
-    >
-      <SunIcon color="var(--text-muted)" />
-      <Switch
-        aria-label="Toggle Light and Dark Mode"
-        {...props}
-        onChange={toggleTheme}
+    <Form autoComplete="off">
+      <div
         css={{
           display: 'inline-flex',
           alignItems: 'center',
+          gap: '4px',
+          transition: 'opacity 0.3s ease',
         }}
-        isSelected={theme === 'dark'}
-        isDisabled={!isThemeSet}
+        style={{ opacity: isThemeSet ? 1 : 0.3 }}
+        aria-hidden={!isThemeSet}
       >
-        {theme === 'dark' ? <ToggleRightIcon /> : <ToggleLeftIcon />}
-      </Switch>
-      <MoonStarsIcon color="var(--text-muted)" />
-    </div>
+        <SunIcon color="var(--text-muted)" />
+        <Switch
+          aria-label="Toggle Light and Dark Mode"
+          {...props}
+          onChange={toggleTheme}
+          css={{
+            display: 'inline-flex',
+            alignItems: 'center',
+          }}
+          isSelected={theme === 'dark'}
+          isDisabled={!isThemeSet}
+        >
+          {theme === 'dark' ? <ToggleRightIcon /> : <ToggleLeftIcon />}
+        </Switch>
+        <MoonStarsIcon color="var(--text-muted)" />
+      </div>
+    </Form>
   )
 }
 
