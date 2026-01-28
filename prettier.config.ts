79cf09809a18b3ed760b4b1c527410f5bf23da9b
Christopher Anderson <src@christopheranderson.net>
2026-01-28 12:03:46 -0500

    Format sql files

--- prettier.config.ts
+++ prettier.config.ts
@@ -4,6 +4,15 @@ const config: Config = {
   semi: false,
   singleQuote: true,
   trailingComma: 'all',
+  plugins: ['prettier-plugin-sql-cst'],
+  overrides: [
+    {
+      files: '*.sql',
+      options: {
+        parser: 'postgresql',
+      },
+    },
+  ],
 }
 
 export default config
